7938ca9deeb2122da91e80ef08b8b90018ac638e
globant-piyush <piyush.jeswani@globant.com>
2019-02-09 16:44:26 +0530

    Another Backup with Uipath

---
+++ ProtractorCodePractise/LearnFirst.js
@@ -0,0 +1,27 @@
+// class First{
+
+//     foo(){
+//         console.log("Hello");
+//     }
+// }
+
+// let first1 = new First();
+// first1.foo();
+
+class BusTrain{
+
+    sarkari(){
+        console.log("..u r in sarkari");
+
+    }
+
+    privateBus(){
+        console.log("...ticket check please");
+
+    }
+}
+
+obj1 = new BusTrain();
+
+//obj1.sarkari();
+obj1.privateBus();
