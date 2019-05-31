b8f9b03db4532521627122f281e18b2d4af959d6
Piyush Jeswani <30585789+Piyush-Jeswani@users.noreply.github.com>
2019-05-31 17:11:39 +0530

    Delete testPageObject.js

--- LearnJS/testPageObject.js
+++
@@ -1,9 +0,0 @@
-var loginPage = require('../Tyco/loginPage.js');
-describe('test page object',function(){
-it('page object', function(){
-    loginPage.openBrowser();
-    loginPage.enterLogin('development','myD3v3l0pm3ntP4ssw0rd');
-    loginPage.clickSubmitBtn();
-});
-
-});
